c2f860c1fb5f21b308465d844b49d032c1ed321c
Dankovtsev Valentin <dankovcev2013@gmail.com>
2020-12-09 18:22:55 +0700

    setting up the environment without husky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    'airbnb-base',"eslint-config-prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-plusplus": "off",
+    "max-len": ["error", { code: 400, ignoreComments: true }],
+    "no-restricted-globals": "off",
+    "no-console": "off",
+    "linebreak-style": "off",
+    "no-restricted-syntax": "off",
+    "no-alert": "off",
+    "no-shadow": "off",
+    "consistent-return": "off",
+  },
+  plugins: ["jest"],
+};
